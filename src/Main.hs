06a3911c52b10e6f86a634fcaca833ad45b9b443
assem abozaid <assemabozaidfcih@gmail.com>
2019-04-18 23:46:42 +0200

    Make it a little clear

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,12 @@ import Game
 import Logic
 import Rendering
 
-window = InWindow "Functional" (screenWidth, screenHeight) (100, 100)
+window = InWindow "Our PL3 Project" (screenWidth, screenHeight) (100, 100)
 backgroundColor = makeColor 255 255 255 255
 
+-- 30 = Number of simulation steps to take for each second of real time.
+-- initialGame = A function to convert the world a picture. (Picture is a datatype which gloss can convert it into GUI )
+-- gameAsPicture = it must handle input events
+-- transformGame = something like reload the picture with the new updated state (when you click to add a X, it envoked to one iteration until an event occured)
 main :: IO ()
 main = play window backgroundColor 30 initialGame gameAsPicture transformGame (const id)
